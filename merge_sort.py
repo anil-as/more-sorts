459cf5208a309763b57718407697a07020ccd753
XPECTO-7 <axpt74@gmail.com>
2025-03-14 14:54:39 +0530

    Added merge sort

---
+++ merge_sort.py
@@ -0,0 +1,28 @@
+
+# MERGE SORT
+
+def merge_sort(arr):
+    if len(arr) <= 1: 
+        return arr
+    mid=len(arr)//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    sorted_arr = []
+    i = j = 0 
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_arr.append(left[i])
+            i += 1
+        else:
+            sorted_arr.append(right[j])
+            j += 1
+    sorted_arr.extend(left[i:])
+    sorted_arr.extend(right[j:])
+    return sorted_arr 
+
+arr = [8, 4, 7, 3, 1, 5, 9, 2, 6]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)  
